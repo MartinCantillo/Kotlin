0f2ed0e50706f48200ff9fdd67b6dcd86f435b43
Martin Cantillo <martinjosecantillohernandez9@gmail.com>
2024-08-22 17:45:38 -0500

    variables

---
+++ HelloWord.kt
@@ -0,0 +1,13 @@
+fun main() {
+
+   //Variables
+   //var mutables, val inmutables
+    val age= 56
+    val customers=60
+    var MartinAge: Int = 21
+    println("the are $customers at this moment")
+    //age=45 error
+    println(age)
+
+
+}

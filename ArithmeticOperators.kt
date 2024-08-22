4f7c0d6ab997e015cc107389ce96a117825f58da
Martin Cantillo <martinjosecantillohernandez9@gmail.com>
2024-08-22 18:01:01 -0500

    ArithmeticOperators

---
+++ ArithmeticOperators.kt
@@ -0,0 +1,15 @@
+fun main(){
+
+    var num1 :Int =5
+    var num2 :Int =10
+    var sum =0
+    sum = num1 + num2
+    println("la suma de $num1 + $num2 es $sum ")
+
+    var num3 :Double = 4.5
+    var num4 :Double = 6.7
+    var sum2 = num3+num4
+   
+    println("La suma es $sum2")
+
+}
